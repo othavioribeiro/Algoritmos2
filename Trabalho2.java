29b72f71db3a569a1ae2c1321af0e0a9a999c3b9
othavioribeiro <othaviobianco@gmail.com>
2018-11-11 15:53:49 -0200

    Update Trabalho2.java

--- Trabalho2.java
+++ Trabalho2.java
@@ -1,4 +1,4 @@
-import javax.swing.JoptionPane;
+import javax.swing.*;
 
 
 public class Trabalho2 {
@@ -6,45 +6,24 @@ public class Trabalho2 {
 	static class Conta {
 		String titular;
 		int numConta;
-		int tipoConta = 0;
-		float saldo = 0;
+		float saldo;
+	}
 		
-	}
-	
-	static Conta criaConta(Conta matrizContas){
-		matrizContas = JoptionPane.showInputDialog("Informe o nome do Titular da conta : ");
-		numConta = Integer.parseInt(JoptionPane.showInputDialog("Informe o numero da conta : "));
-		tipoConta = Integer.parseInt(JoptionPane.showInputDialog("Informe 1 - Conta Corrente ou 2 - Conta Poupança : "));
-		if(tipoConta == 1){
-			static float limite = Float.parseFloat(JoptionPane.showInputDialog("Informe o limite da conta: "));
-	}
-	
-	static float deposito(Conta matrizContas, float valor){
-		matrizContas.saldo = matrizContas.saldo + valor;
-		return matrizContas.saldo;
-	}
-	static float saque(){
-		matrizContas.saldo = matrizContas.saldo - valor;
-		return matrizContas.saldo;
-	}
-	static float transferencia(Conta matrizContas1, Conta matrizContas2, float valor){
-		matrizContas1.saldo = matrizContas1.saldo - valor;
-		matrizContas2.saldo = matrizContas2.saldo + valor;
-	}
-	static void saldo(Conta matrizContas){
-		return matrizContas.saldo;
-	}
-	
-	static Conta retornaConta(Conta matrizContas, numConta, n){
-		for(i=0; i<n; i++){
-			if(numConta == matrizContas.numConta)
-				return matrizContas;
-	}
+	public static void criarVetor(){
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Informe o numero de contas a serem cadastradas: "));
+		Conta vetorContas[] = new Conta[n];
+		
+        for(int i=0; i<n; i++){
+            vetorContas[i].titular = JOptionPane.showInputDialog(null, "Nome do titular: ");
+            
+            vetorContas[i].numConta = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o numero da conta: "));
+            
+            vetorContas[i].saldo = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite o saldo inicial da conta: "));
+        }
+    }
 	
 	public static void main (String[] args) {
-		
-		
+		criarVetor();
 		
 	}
 }
-
