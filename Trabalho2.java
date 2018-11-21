ccd6c35c99383a3cd6618d743f1d984951c63f41
gabrielgs092 <gabrielgs092@gmail.com>
2018-11-20 23:56:32 -0200

    Indentação do código e leitura do arquivo para o programa

--- Trabalho2.java
+++ Trabalho2.java
@@ -1,29 +1,21 @@
+import javax.swing.*;
+import java.io.*;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import javax.swing.JOptionPane;
 public class Trabalho2 {
-    static class Conta {
-        String titular;
-        int numConta;
-        float saldo;
-        String extrato;
-    }
+
     static int tam = 0;
     static Conta vetorContas[] = new Conta[10];
-    public static void criaConta(String nome, int num) {
+
+    public static void criaConta(String nome, int num, float saldo) {
         Conta nova = new Conta();
         nova.titular = nome;
         nova.numConta = num;
+        nova.saldo = saldo;
         vetorContas[tam] = nova;
         tam++;
     }
-    static void transferencia(int numContDoador, int numContRecebedor, float valorTran){
+
+    static void transferencia(int numContDoador, int numContRecebedor, float valorTran) {
         int indiceDoDoador = -1;
         int indiceDoRecebedor = -1;
         for (int i = 0; i < tam; i++) {
@@ -37,37 +29,41 @@ public class Trabalho2 {
                 indiceDoRecebedor = i;
             }
         }
-        
-        if(indiceDoDoador!=-1&&indiceDoRecebedor!=-1){
+
+        if (indiceDoDoador != -1 && indiceDoRecebedor != -1) {
             vetorContas[indiceDoDoador].saldo = vetorContas[indiceDoDoador].saldo - valorTran;
             vetorContas[indiceDoRecebedor].saldo = vetorContas[indiceDoRecebedor].saldo + valorTran;
         }
     }
-    static void deposito(int numConta, float valor){
-        for (int i =0; i<tam; i++){
-            if(numConta == vetorContas[i].numConta){
+
+    static void deposito(int numConta, float valor) {
+        for (int i = 0; i < tam; i++) {
+            if (numConta == vetorContas[i].numConta) {
                 vetorContas[i].saldo += valor;
             }
-        } 
+        }
     }
-    static void saque(int numConta, float valor){
-        for (int i =0; i<tam; i++){
-            if(numConta == vetorContas[i].numConta){
+
+    static void saque(int numConta, float valor) {
+        for (int i = 0; i < tam; i++) {
+            if (numConta == vetorContas[i].numConta) {
                 vetorContas[i].saldo = vetorContas[i].saldo - valor;
             }
-        } 
+        }
     }
-    static float saldo(int numConta){
-		float result = 0;
-        for (int i=0;i<tam;i++){
-			System.out.printf("%d: %d %f\n", i, vetorContas[i].numConta, vetorContas[i].saldo);
-            if(numConta == vetorContas[i].numConta){
+
+    static float saldo(int numConta) {
+        float result = 0;
+        for (int i = 0; i < tam; i++) {
+            System.out.printf("%d: %d %f\n", i, vetorContas[i].numConta, vetorContas[i].saldo);
+            if (numConta == vetorContas[i].numConta) {
                 result = vetorContas[i].saldo;
             }
         }
         return result;
     }
-      public static void escreverArquivo(String nome) {
+
+    public static void escreverArquivo(String nome) {
         FileWriter fileWriter = null;
         BufferedWriter arquivo = null;
         String caminho = "arquivo.txt";
@@ -76,11 +72,11 @@ public class Trabalho2 {
             if (!file.exists()) {
                 file.createNewFile();
             }
-            fileWriter = new FileWriter(caminho ,true);
+            fileWriter = new FileWriter(caminho, true);
             arquivo = new BufferedWriter(fileWriter);
 
             for (int i = 0; i < tam; i++) {
-                if (vetorContas[i].titular.equals(nome))  {
+                if (vetorContas[i].titular.equals(nome)) {
                     arquivo.write(vetorContas[i].titular + ";" + vetorContas[i].numConta + ";" + vetorContas[i].saldo);
                     arquivo.newLine();
                 } else {
@@ -101,29 +97,24 @@ public class Trabalho2 {
         }
 
     }
-       public static String lerArquivo() {
-           String retorno = "";
-			FileReader fileReader = null;
-			BufferedReader arquivo = null;
-			try {
+
+    public static void lerArquivo() {
+        FileReader fileReader = null;
+        BufferedReader arquivo = null;
+        try {
             fileReader = new FileReader("arquivo.txt");
             arquivo = new BufferedReader(fileReader);
 
             String leitura = arquivo.readLine();
-            
-            while (leitura != null){    
 
+            while (leitura != null) {
                 String vet[] = leitura.split(";");
 
-                for (String v : vet) {
-
-                    retorno += v + " ";
+                if (vet.length > 2) {
+                    criaConta(vet[0], Integer.parseInt(vet[1]), Float.parseFloat(vet[2]));
                 }
-                
-                retorno += "\n";
 
                 leitura = arquivo.readLine();
-                
             }
 
             arquivo.close();
@@ -138,12 +129,11 @@ public class Trabalho2 {
             } catch (Exception e) {
             }
         }
-        return retorno;
     }
 
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         int op;
-        do { 
+        do {
             op = Integer.parseInt(JOptionPane.showInputDialog("Selecione uma opcao: \n"
                     + "1) Criar Conta \n"
                     + "2) Depositar \n"
@@ -153,35 +143,35 @@ public class Trabalho2 {
                     + "6) Gravar usuario \n"
                     + "7) Ler usuario \n"
                     + "8) Sair"));
-            if(op == 1){
+            if (op == 1) {
                 String nome = JOptionPane.showInputDialog("Insira o nome do Titular");
                 int num;
                 num = Integer.parseInt(JOptionPane.showInputDialog("Insira o número da conta"));
-                criaConta(nome, num);
+                criaConta(nome, num, 0);
             }
-            if(op == 2){
+            if (op == 2) {
                 int numConta = Integer.parseInt(JOptionPane.showInputDialog("Insira o número da conta"));
-                float valor  = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor do depósito"));
+                float valor = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor do depósito"));
                 deposito(numConta, valor);
             }
-            if(op == 3){
+            if (op == 3) {
                 int numConta = Integer.parseInt(JOptionPane.showInputDialog("Insira o número da conta"));
-                float valor  = Float.parseFloat(JOptionPane.showInputDialog("Insira o valor do saque"));
+                float valor = Float.parseFloat(JOptionPane.showInputDialog("Insira o valor do saque"));
                 saque(numConta, valor);
             }
-            if(op ==4){
+            if (op == 4) {
                 int numContDoador = Integer.parseInt(JOptionPane.showInputDialog("Insira o número da conta do Doador"));
                 int numContRecebedor = Integer.parseInt(JOptionPane.showInputDialog("Insira o número da conta do Recebedor"));
                 float valorTran = Float.parseFloat(JOptionPane.showInputDialog("Insira o valor da transação"));
                 transferencia(numContDoador, numContRecebedor, valorTran);
             }
-            if(op ==5){
+            if (op == 5) {
                 float saldo = 0;
                 int numConta = Integer.parseInt(JOptionPane.showInputDialog("Insira o número da conta"));
                 saldo = saldo(numConta);
-                JOptionPane.showMessageDialog(null,saldo);
+                JOptionPane.showMessageDialog(null, saldo);
             }
-            if(op ==6){
+            if (op == 6) {
                 String nome = JOptionPane.showInputDialog("Insira o nome do titular da conta a ser gravada");
                 int a = 0;
                 for (int i = 0; i < tam; i++) {
@@ -191,9 +181,16 @@ public class Trabalho2 {
                 }
                 escreverArquivo(vetorContas[a].titular);
             }
-            if(op ==7){
+            if (op == 7) {
                 lerArquivo();
             }
-        }while(op !=8);
+        } while (op != 8);
+    }
+
+    static class Conta {
+        String titular;
+        int numConta;
+        float saldo;
+        String extrato;
     }
 }
